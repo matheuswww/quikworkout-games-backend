4452ab55014b7205ee3c5453d73cf15383c47d0a
matheuswww <teteus.30.08.07@outlook.com>
2025-05-31 19:00:45 -0300

    checking if user exists in enterAccount

--- src/model/user/repository/enterAccount.go
+++ src/model/user/repository/enterAccount.go
@@ -2,6 +2,7 @@ package user_repository
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/matheuswww/quikworkout-games-backend/src/configuration/logger"
@@ -13,8 +14,19 @@ import (
 func (ur *userRepository) EnterAccount(userDomain user_domain.UserDomainInterface) *rest_err.RestErr {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
-	query := "UPDATE user_games SET session_id = ? WHERE user_id = ?"
-	_,err := ur.mysql.ExecContext(ctx, query, userDomain.GetSessionId(), userDomain.GetId())
+	query := "SELECT COUNT(*) FROM user_games WHERE user_id = ?"
+	var count int
+	err := ur.mysql.QueryRowContext(ctx, query, userDomain.GetId()).Scan(&count)
+	if err != nil {
+		logger.Error("Error trying QueryRowContext", err, zap.String("journey", "EnterAccount Repository"))
+		return rest_err.NewInternalServerError("server error")
+	}
+	if count == 0 {
+		logger.Error("Error User not found", errors.New("user not found"), zap.String("journey", "EnterAccount Repository"))
+		return rest_err.NewBadRequestError("usuário não encontrado")
+	}
+	query = "UPDATE user_games SET session_id = ? WHERE user_id = ?"
+	_,err = ur.mysql.ExecContext(ctx, query, userDomain.GetSessionId(), userDomain.GetId())
 	if err != nil {
 		logger.Error("Error trying ExecContext", err, zap.String("journey", "EnterAccount Repository"))
 		return rest_err.NewInternalServerError("server error")
