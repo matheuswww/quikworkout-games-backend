5d3bb3e05d1b6fe896943ddca55b67f4d7e13bf1
matheuswww <teteus.30.08.07@outlook.com>
2025-07-05 20:31:02 -0300

    added validator in category field

--- src/controller/model/user/request/update.go
+++ src/controller/model/user/request/update.go
@@ -5,7 +5,7 @@ import "mime/multipart"
 type Update struct {
 	User     string                `form:"user" binding:"omitempty,min=1,max=30" validate:"user"`
 	Name     string                `form:"name" binding:"omitempty,min=2,max=20"`
-	Category string                `form:"category" binding:"omitempty,max=10"`
+	Category string                `form:"category" binding:"omitempty,max=10" validate:"category"`
 	Token    string                `form:"token" binding:"max=10000"`
 	Image    *multipart.FileHeader `form:"imagem"`
 }
